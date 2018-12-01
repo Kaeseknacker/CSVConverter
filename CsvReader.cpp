eb752337f52bb774450035d64ff7feb1d4f91c87
unknown <Raphaelspraul@hotmail.de>
2018-12-01 11:59:43 +0100

    Sparkassen CSV einlesen

--- CsvReader.cpp
+++ CsvReader.cpp
@@ -40,6 +40,7 @@ QList<AccountingEntry> CsvReader::readAccountStatementComdirect(QString filePath
 
         QList<QByteArray> columns = line.split(';');
 
+        // TODO: Es gibt keine Backslashes. Steige hier auf QString um
         QByteArray first = columns[0];
 
         first.replace('.', '/');
@@ -77,9 +78,44 @@ QList<AccountingEntry> CsvReader::readAccountStatementComdirect(QString filePath
 
 QList<AccountingEntry> CsvReader::readAccountStatementSparkasse(QString filePath)
 {
-    Q_UNUSED(filePath)
+    QFile file(filePath);
+    if (!file.open(QIODevice::ReadOnly)) {
+        qDebug() << file.errorString();
+        return QList<AccountingEntry>();
+    }
 
-    // TODO: Implementieren
+    QList<AccountingEntry> entries;
 
-    return QList<AccountingEntry>();
+    while (!file.atEnd()) {
+        QByteArray line = file.readLine();
+
+        QList<QByteArray> columns = line.split(';');
+
+        // Entferne die Anfuehrungszeichen und passe das Datumsformat an
+        QString second = columns[1].replace("\"", "").replace(".", "/");
+        // NOTE: Hack, alle Buchungen erst nach 2000
+        second.insert(6, "20");
+
+        QDate date = QDate::fromString(second, "dd/MM/yyyy");
+
+        if (date.isValid()) {
+
+            QString description = columns[4].replace("\"", "") + " " + columns[5].replace("\"", "");
+
+            QString sAmount = columns[8].replace("\"", "").replace(",", ".");
+
+            float amount = sAmount.toFloat();
+
+            AccountingEntry entry;
+            entry.setAccountingDate(date);
+            entry.setDescription(description.simplified());
+            entry.setAmount(amount);
+
+            entries.push_back(entry);
+        }
+    }
+
+    file.close();
+
+    return entries;
 }
